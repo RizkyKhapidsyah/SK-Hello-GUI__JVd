d553c9dd5e1167c11539be2f770aec7ffd20fa86
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-12-11 08:13:13 +0700

    Initial commit

---
+++ src/com/rk/HelloGUI.java
@@ -0,0 +1,15 @@
+
+package com.rk;
+
+import javax.swing.*;
+
+public class HelloGUI {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello World");
+        frame.setSize(300, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
